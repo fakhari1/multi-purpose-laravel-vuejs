bc76cb48c069e91cea53dff369e53db5dac2448c
fakhari1 <hosein.fakhari1998@gmail.com>
2022-11-10 12:42:36 +0330

    apply some changes in user table and user factory and user seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -20,5 +21,16 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        $admin = $this->createUser(['first_name' => 'Admin', 'email' => 'admin@admin.com']);
+        $admin = $this->createUser(['first_name' => 'Manager', 'email' => 'manager@manager.com']);
+        $admin = $this->createUser(['first_name' => 'Supervisor', 'email' => 'supervisor@supervisor.com']);
+        $admin = $this->createUser(['first_name' => 'Support', 'email' => 'support@support.com']);
+        $admin = $this->createUser(['first_name' => 'Client', 'email' => 'client@client.com']);
+    }
+
+    public function createUser($attributes)
+    {
+        return User::factory()->create($attributes);
     }
 }
